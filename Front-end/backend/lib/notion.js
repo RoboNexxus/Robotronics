f71db896cb4481913f0704819be0058689592b5d
Atharv Mandlavdiya <atharvam682@gmail.com>
2026-07-03 17:29:39 +0530

    skip member_3 for robo race and line follower

--- Front-end/backend/lib/notion.js
+++ Front-end/backend/lib/notion.js
@@ -30,20 +30,25 @@ async function createRegistration(databaseId, data) {
   } = data;
 
   try {
+    const properties = {
+      "Name":          { title: [{ text: { content: leaderName } }] },
+      "Team Name":     { rich_text: [{ text: { content: teamName } }] },
+      "Reg ID":        { rich_text: [{ text: { content: regId } }] },
+      "School":        { rich_text: [{ text: { content: school } }] },
+      "Email":         { email },
+      "Phone":         { phone_number: phone },
+      "Discord ID":    { rich_text: [{ text: { content: discordId || "N/A" } }] },
+      "Team Size":     { number: Number(teamSize) },
+      "member_2":      { rich_text: [{ text: { content: member2 || "" } }] },
+    };
+
+    if (eventName !== "Robo Race" && eventName !== "Line Follower") {
+      properties["member_3"] = { rich_text: [{ text: { content: member3 || "" } }] };
+    }
+
     await notion.pages.create({
       parent: { database_id: databaseId },
-      properties: {
-        "Name":          { title: [{ text: { content: leaderName } }] },
-        "Team Name":     { rich_text: [{ text: { content: teamName } }] },
-        "Reg ID":        { rich_text: [{ text: { content: regId } }] },
-        "School":        { rich_text: [{ text: { content: school } }] },
-        "Email":         { email },
-        "Phone":         { phone_number: phone },
-        "Discord ID":    { rich_text: [{ text: { content: discordId || "N/A" } }] },
-        "Team Size":     { number: Number(teamSize) },
-        "member_2":      { rich_text: [{ text: { content: member2 || "" } }] },
-        "member_3":      { rich_text: [{ text: { content: member3 || "" } }] },
-      },
+      properties: properties,
     });
     return true;
   } catch (err) {
